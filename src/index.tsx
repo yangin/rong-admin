28fcde43c2ee11cab53756ee9e65fad8224a99bd
joyce <joyce.fc.dev@gmail.com>
2023-07-26 21:41:49 +0800

    FEAT: 移除strict模式

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,8 @@
-import React, { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './pages'
 import './index.less'
 
 const root = createRoot(document.getElementById('root') as HTMLDivElement)
 
-// root.render(<App />)
-
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-)
+root.render(<App />)
